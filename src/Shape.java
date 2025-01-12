ae378e22a96da24feb6b2bd4cedf104795db73e2
Htun Thiha <htunthiha.dev@gmail.com>
2025-01-12 20:47:15 +0800

    Add Shape class
    Shape class has static method getShapesList that returns String list of rock, paper, scissors

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shape {
+
+    public static List<String> getShapesList() {
+        var shapesList = new ArrayList<>(List.of("rock", "paper", "scissors"));
+        return shapesList;
+    }
 }
