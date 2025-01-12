b0d1de78dad3108219ece2a50ffa2749b319c419
Htun Thiha <htunthiha.dev@gmail.com>
2025-01-12 20:54:52 +0800

    Modify RockPaperScissors
    compare function added

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class RockPaperScissors {
+
+    public static String compare(String p_finalShape, String b_finalShape) {
+        var shapesList = Shape.getShapesList();
+
+        int p_index = shapesList.indexOf(p_finalShape);
+        int b_index = shapesList.indexOf(b_finalShape);
+        int result = (p_index - b_index + 3) % 3;
+
+        if (result == 0) {
+            return "Draw";
+        } else if (result == 1) {
+            return "Player Wins";
+        } else {
+            return "Bot Wins";
+        }
+    }
 }
