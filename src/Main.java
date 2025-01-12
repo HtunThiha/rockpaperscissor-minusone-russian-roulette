239cecdc64650fdf3d6f06abbd279f4384bf6df2
Htun Thiha <htunthiha.dev@gmail.com>
2025-01-12 19:46:09 +0800

    Add Revolver class
    Revolver class: setDeathRounds fill bullets in random rounds, number of bullets is specified in parameter
    
    spinAndfire chooses a random round and fires (returns true for hit and false for not hit)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Revolver.setDeathRounds(3);
+        System.out.println(Revolver.spinAndFire());
     }
 }
---
+++ src/Revolver.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Revolver {
+
+    private static List<Integer> deathRounds = new ArrayList<>();
+
+    public static void setDeathRounds(int numOfBullets) {
+        deathRounds = new ArrayList<>();
+        List<Integer> bulletRounds = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+
+        for (int i = 0; i < numOfBullets; i++) {
+            int selectedRoundIndex = (int) Math.floor(Math.random() * bulletRounds.size());
+            int liveRound = bulletRounds.get(selectedRoundIndex);
+            deathRounds.add(liveRound);
+            bulletRounds.remove(selectedRoundIndex);
+        }
+        System.out.println("deathRounds: " + deathRounds);
+    }
+
+    public static boolean spinAndFire() {
+        int triggeredRound = 1 + (int) Math.floor(Math.random() * 6);
+        System.out.println("triggeredRound: " + triggeredRound);
+        if (deathRounds.contains(triggeredRound)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
