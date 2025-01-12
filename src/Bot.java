3520e65146f1f9695356d10e694d94618b8c0257
Htun Thiha <htunthiha.dev@gmail.com>
2025-01-12 20:49:15 +0800

    Add Bot class
    Bot selects two shapes, and choose one out of two, and returns respective choices to main program when called

--- src/Bot.java
+++ src/Bot.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Bot {
+
+    public static List<String> selectTwoShapes() {
+        List<String> shapesList = Shape.getShapesList();
+
+        int shape1Index = (int) Math.floor(Math.random() * 3);
+        String shape1 = shapesList.get(shape1Index);
+        shapesList.remove(shapesList.indexOf(shape1));
+
+        int shape2Index = (int) Math.floor(Math.random() * 2);
+        String shape2 = shapesList.get(shape2Index);
+
+        return List.of(shape1, shape2);
+    }
+
+    public static String selectOneShape(List<String> shapesList) {
+        int indexToSelect = (int) Math.floor(Math.random() * 2);
+        return shapesList.get(indexToSelect);
+    }
 }
